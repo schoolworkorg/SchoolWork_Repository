b132b144c1f6c7652a1281143a570850ea3861fd
schoolworkorg <118563969+schoolworkorg@users.noreply.github.com>
2023-06-01 21:55:16 +0400

    Add files via upload

---
+++ GamopenebiPage.php
@@ -0,0 +1,32 @@
+<?php
+error_reporting (E_ALL ^ E_NOTICE);
+error_reporting(0);
+?>
+
+
+
+
+<!DOCTYPE html>
+<html>
+<head>
+<title>გომეფენები - SchoolWork</title>
+<link type="text/css" rel="stylesheet" href="SchoolWork_styles/GamopenebiPage_GamopenebiGverdi_Style.css"; />
+<link type="image/png" rel="icon" href="SchoolWork_images/SchoolWorkLogo.PNG"; />
+<meta name="viewport"content="initial-scale=1width=device-width"; />
+<script src="Scripts/GamopenebiPage_Script.js"></script>
+<script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-7159378308438732"
+     crossorigin="anonymous"></script>
+<link type="text/css" rel="stylesheet" href="schoolwork_oneforall_styles/darktheme_style.css"; />
+<script src="GamopenebiPage_script.js"; ></script>
+<script src="jquery.js"; ></script>
+<link rel="stylesheet" type="text/css" href="schoolwork_oneforall_styles/NamushevrebiPublic_style.css"; />
+</head>
+<body>
+<center id="GamopenebiPage_Title_ID" class="DarkThemeTextBOLD"; >
+გამოფენების გვერდი / გამოფენები
+</center>
+<br>
+<br>
+<br>
+</body>
+</html>
